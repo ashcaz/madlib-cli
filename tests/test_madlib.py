d078daf57827b988b39350902182889c81e1da14
Ashley Casimir <ashcas@me.com>
2020-12-13 20:15:01 -0800

    test_merge wont pass because i am getting user input and replacing text in one function. Test passes when i use -s flag with pytest and input the words myself

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,24 +1,17 @@
 import pytest
+import unittest.mock
 from madlib_cli import __version__
-from  madlib_cli.madlib import read_template, parse_template,merge
+from  madlib_cli.madlib import read_template, parse_template,merge, write_template
 
 def test_version():
     assert __version__ == '0.1.0'
 
-# def test_read_file_test_output():
-#   actual = read_file('../assets/test_text.txt')
-#   expected = 'I the {Adjective} and {Adjective} {A First Name}'
-#   assert actual == expected 
 
-# def test_write_to_new_file_test_output():
-#   actual = write_to_new_file(read_file('../assets/test_text.txt'))
-#   expected = '../assets/final_madlib'
-#   assert actual == expected 
+def test_write_template_output():
+  actual = write_template("It was a {Adjective} and {Adjective} {Noun}.")
+  expected = '../assets/final_madlib'
+  assert actual == expected 
 
-# def test_parse_file():
-#   actual = parse_file(read_file('../assets/test_text.txt'))
-#   expected = 'I the funny and slow Ashley'
-#   assert actual == expected 
 
 def test_read_template_returns_stripped_string():
     actual = read_template("../assets/dark_and_stormy_night.txt")
@@ -37,7 +30,7 @@ def test_parse_template():
     assert actual_parts == expected_parts
 
 
-def test_merge():
+def test_merge(monkeypatch):
     actual = merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
     expected = "It was a dark and stormy night."
     assert actual == expected
